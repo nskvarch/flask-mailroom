627a4ba832fc3c9e35d729560d60afb126625e8c
Niels Skvarch <nielsk01@noa.nintendo.com>
2020-12-09 15:13:34 -0800

    fixed secret key lookup again

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from model import Donation, Donor, User
 
 app = Flask(__name__)
-app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+# app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
 
-# app.secret_key = os.environ.get("SECRET_KEY").encode()
+app.secret_key = os.environ.get("SECRET_KEY").encode()
 
 
 @app.route("/")
